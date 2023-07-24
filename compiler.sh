ed8910cb2897f008bac3fb42afa645065a9510de
Gabriel Coelho Costa <coelhocostag@gmail.com>
2023-07-23 23:02:14 -0300

    Add SFML static library

--- compiler.sh
+++ compiler.sh
@@ -1,3 +1,4 @@
-g++ -c main.cpp
-g++ main.o -o bin/test -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
-./bin/test
+g++ -c main.cpp -I./SFML/include
+g++ main.o -o bin/app -L./SFML/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
+export LD_LIBRARY_PATH=./SFML/lib
+./bin/app
